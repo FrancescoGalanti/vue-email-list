bc9904d7ab14455510d15570d70768cfa46b26b4
FrancescoGalanti <galanti_francesco>
2020-11-23 16:57:25 +0100

    esercizio fatto

--- javascript/main.js
+++ javascript/main.js
@@ -5,38 +5,32 @@ var app = new Vue({
     el: '#app',
     data: {
 
-     lista3: []
+       lista3: []
    },
 
    created(){
-
+      // Reference to change the name of this in self to avoid scoping the window site in axios.get //
       const self = this;
+      // calling ten times the axios.get //
+      for (i = 0; i < 10 ; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            // Methods inside get //
+            .then(function (response) {
+                // Testing the call API made with JASON //
+                console.log(response)
+                // reference using response.data.response //
+                 self.item = response.data.response
+                 // Testing //
+                 console.log(self.item)
+                 // Pushing the object email in lista3 //
+                 self.lista3.push(self.item);
+            })
+            .catch(function (error) {
+                alert(error);
+            })
+        }
 
-       axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-         .then(function (response) {
 
-            self.data = response.data;
-
-            self.data.forEach((item) => {
-              self.lista3.push(item)
-            });
-
-         })
-
-          .catch(function (error) {
-          // handle error
-          console.log(error);
-         });
-
-
-      },
+    }
 
 });
-
-
-// const list = [7,8,9]
-// const list2 = ["a","b","c"]
-//
-//
-// var newArray = list.map((e, i) => e + list2[i]);
-// console.log(newArray);
